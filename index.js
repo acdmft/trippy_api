cc55c5cf77a47b3095ad3cb2370368edbfa354ae
fusbudget <andrewsanser@gmail.com>
2022-03-14 13:52:58 +0100

    add get all hotels route in the hotelsRoutes.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+// ROUTES 
+const hotelsRoutes = require("./routes/hotelsRoutes");
+
+app.use(express.json());
+
+app.use("/hotels", hotelsRoutes);
+
+app.use("*", (err, req, res, next) => {
+  res.status(404).send("404 error");
+})
+app.listen(8080, () => console.log("Listent port 8080..."));
---
+++ routes/hotelsRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router(); 
+
+// HOTELS 
+const hotels = 
+[
+	{
+		"id": 1,
+		"name": "Imperial Hotel",
+		"address": "84 av des Champs-Élysées",
+		"city": "Paris",
+		"country": "France",
+		"stars": 5,
+		"hasSpa": true,
+		"hasPool": true,
+		"priceCategory": 3
+	},
+	{
+		"id": 2,
+		"name": "The Queen",
+		"address": "3 Darwin Street",
+		"city": "London",
+		"country": "England",
+		"stars": 4,
+		"hasSpa": true,
+		"hasPool": false,
+		"priceCategory": 3
+	},
+	{
+		"id": 3,
+		"name": "Kiwi land",
+		"address": "4587 George St.",
+		"city": "Auckland",
+		"country": "New-Zealand",
+		"stars": 3,
+		"hasSpa": false,
+		"hasPool": true,
+		"priceCategory": 2
+	}
+]
+
+// GET ALL HOTELS 
+router.get("/", (_req, res) => {
+  res.json(hotels);
+})
+
+module.exports = router;
